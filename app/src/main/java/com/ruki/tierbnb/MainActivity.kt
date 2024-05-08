3d8cbf3c6f59306f885373915400ae9da36005a4
Pruskan <petarruskan@gmail.com>
2024-05-08 19:46:40 +0200

    Tierbnb register page

--- app/src/main/java/com/ruki/tierbnb/MainActivity.kt
+++ app/src/main/java/com/ruki/tierbnb/MainActivity.kt
@@ -4,26 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,11 +31,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -60,13 +56,13 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
 
-                    NavHost(navController = navController, startDestination = "main_screen") {
-                        composable("main_screen") {
-                            MainScreen(navController = navController)
+                    NavHost(navController = navController, startDestination = "login_screen") {
+                        composable("login_screen") {
+                            LoginScreen(navController = navController)
                         }
-                        /*composable("step_counter") {
-                            StepCounter(navController = navController)
-                        }*/
+                        composable("register_screen") {
+                            RegisterScreen(navController = navController)
+                        }
                     }
                 }
             }
@@ -86,79 +82,169 @@ fun BackgroundImage(modifier: Modifier, @DrawableRes imageResource: Int) {
     }
 }
 
-@Composable
+/*@Composable
 fun MainScreen(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment =
     Alignment.Center) {
         BackgroundImage(modifier = Modifier.fillMaxSize(), imageResource = R.drawable.login_background)
-        UserPreview()
+        LoginScreen(navController = navController)
     }
-}
+}*/
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserPreview() {
+fun LoginScreen(navController: NavController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .clip(shape = RoundedCornerShape(20.dp))
-            .background(Color.LightGray.copy(0.5F))
-            .padding(20.dp)
-    ) {
-        Text(
-            text = "PRIJAVA",
-            fontSize = 20.sp,
-            lineHeight = 61.sp,
-            textAlign = TextAlign.Center,
-            color = Color.White
-        )
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            shape = RoundedCornerShape(10.dp),
-            textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = LightBlue,
-                unfocusedBorderColor = Color.White,
-            )
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Lozinka") },
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.padding(vertical = 8.dp),
-            shape = RoundedCornerShape(10.dp),
-            textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = LightBlue,
-                unfocusedBorderColor = Color.White,
-            )
-        )
-        Button(
-            onClick = {
-
-            },
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        BackgroundImage(modifier = Modifier.fillMaxSize(), imageResource = R.drawable.login_background)
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(16.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = LightBlue, contentColor = Color.White)
+                .clip(shape = RoundedCornerShape(20.dp))
+                .background(Color.LightGray.copy(0.5F))
+                .padding(20.dp)
         ) {
-            Text(text = "Prijavi se")
+            Text(
+                text = "PRIJAVA",
+                fontSize = 20.sp,
+                lineHeight = 61.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = LightBlue,
+                    unfocusedBorderColor = Color.White,
+                )
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Lozinka") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.padding(vertical = 8.dp),
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = LightBlue,
+                    unfocusedBorderColor = Color.White,
+                )
+            )
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = LightBlue, contentColor = Color.White)
+            ) {
+                Text(text = "Prijavi se")
+            }
+            ClickableText(text = AnnotatedString("Nemaš račun? registriraj se"), onClick = {
+                navController.navigate("register_screen")
+            })
         }
     }
 }
 
-/*@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TierbnbTheme {
-        Greeting("Android")
+fun RegisterScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordRep by remember { mutableStateOf("") }
+
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        Button(
+            onClick = {
+                navController.navigate("login_screen")
+            },
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp)
+                .background(Color.LightGray.copy(0.5F))
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.baseline_arrow_back_ios_new_24),
+                contentDescription = "Back"
+            )
+        }
+
+        BackgroundImage(modifier = Modifier.fillMaxSize(), imageResource = R.drawable.login_background)
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(20.dp))
+                .background(Color.LightGray.copy(0.5F))
+                .padding(20.dp)
+        ) {
+            Text(
+                text = "REGISTRACIJA",
+                fontSize = 20.sp,
+                lineHeight = 61.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = LightBlue,
+                    unfocusedBorderColor = Color.White,
+                )
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Lozinka") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.padding(vertical = 8.dp),
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = LightBlue,
+                    unfocusedBorderColor = Color.White,
+                )
+            )
+            OutlinedTextField(
+                value = passwordRep,
+                onValueChange = { passwordRep = it },
+                label = { Text("Ponovite lozinku") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.padding(vertical = 8.dp),
+                shape = RoundedCornerShape(10.dp),
+                textStyle = TextStyle(color = Color.White, fontWeight = FontWeight.Bold),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = LightBlue,
+                    unfocusedBorderColor = Color.White,
+                )
+            )
+            Button(
+                onClick = {
+    
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = LightBlue, contentColor = Color.White)
+            ) {
+                Text(text = "Registriraj se")
+            }
+        }
     }
-}*/
+}
